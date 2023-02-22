4ebea91167f909609c6b864feb141ae58330959d
Michal Rusek <michal.rusek@dka.io>
2023-02-22 13:48:01 +0100

    Dicee app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.pink.shade50,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.pink.shade100,
         ),
         body: DicePage(),
       ),
@@ -15,9 +18,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+var leftRandom = 1;
+var rightRandom = 2;
+void randomDice() {
+  leftRandom = Random().nextInt(6) + 1;
+  rightRandom = Random().nextInt(6) + 1;
+}
+
+
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice$leftRandom.png"),
+              onPressed: () {
+                setState(() {
+                  randomDice();
+                });
+
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset("images/dice$rightRandom.png"),
+              onPressed: () {
+                setState(() {
+                  randomDice();
+                });
+
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
